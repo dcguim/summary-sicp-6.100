128da174998261d6c3885046dc0fe9180ade0247
Daniel Guimaraes <dcguim@gmail.com>
2017-07-03 01:00:11 -0300

    Second Lesson:
            Using vecto to handle pictures
    
    	Using rectangles to build images, it is defined this lesson
    	a function to make a rectangle and some accessors to get it`s
    	origin, horizontal value and vertical value it is also defined
    	a coord-map, which returns a procedure as value, in a way that
    	when given a point in the plane, it will scale the rectangles
    	coordinates according to the given point, then add to the
    	origin of the rectangle.
    
    	The goal of this lesson is how expressing several languages
    	which would express several layers of buildin complex
    	pictures. We would have a language for expressing primitive
    	pictures, then on top of it we would express a language for
    	expressing geometric positions and functions which would act
    	on the pictures. Above all we would have schemes of
    	combination based on the geometric functions of the level
    	beneath.
    
    	Reported-by: dcguim
    	Signed-off-by: Daniel C. Guimaraes <dcguim@gmail.com>
    
    Expressing languages to build and handle pictures.

--- l3.lisp
+++ l3.lisp
@@ -22,7 +22,7 @@
 ;;     (rectangle 10 20 30 40)
 ;;     (fill-path)
 ;;     (save-png f))
-
+;; *note that the function above was already defined in lesson 2*
 (defun add-seg (a b)
   (cons (+ (car a) (car b))
 	(+ (cdr a) (cdr b))))
@@ -61,9 +61,81 @@
 ;; (with-open-file (f "~/work/sicp/img.png")
 ;;   (with-canvas (:width 90 :height 90)
 ;;     (drawline (cons 20 30) (cons 40 30))
-;;     (save-png)))
+;;     (save-png f)))
 (defun drawline (p1 p2)
   (with-graphics-state
     (move-to (car p1) (cdr p1))
     (line-to (car p2) (cdr p2))
     (stroke)))
+
+;; https://en.wikipedia.org/wiki/Bézier_curve
+;; http://www.xach.com/lisp/vecto/#curve-to
+(defun drawcurve (p1 p2 p3 p4)
+  (with-graphics-state
+    (move-to (car p1) (cdr p1))
+    (curve-to (car p2) (cdr p2)
+	      (car p3) (cdr p3)
+	      (car p4) (cdr p4))
+    (stroke)))
+
+	     
+
+;; Drawing the letter 'A' with make-picture
+;; (with-open-file (f "~/work/sicp/img.png")
+;;   (with-canvas (:width 300 :height 300)
+;;     (funcall (make-picture (list
+;; 			    (list (cons 30 30)
+;; 				  (cons 150 270))
+;; 			    (list (cons 150 270)
+;; 				  (cons 270 30))
+;; 			    (list (cons 90 150)
+;; 				  (cons 210 150))))
+;; 	     (make-rectangle 0 0 1 1))
+;;     (save-png f)))
+(defun make-picture (seglist)
+  (lambda (rect)
+    (mapcar 
+     (lambda (s)
+       (cond ((= (length  s) 2)		     
+	      (drawline
+	       (funcall (coord-map rect) (car s))
+	       (funcall (coord-map rect) (cadr s))))
+	     ((= (length s) 4)
+	      (drawcurve
+	       (funcall (coord-map rect) (car s))
+	       (funcall (coord-map rect) (cadr s))
+	       (funcall (coord-map rect) (caddr s))
+	       (funcall (coord-map rect) (cadddr s))))))
+     seglist)))
+
+;; Draw 'B' and use it in beside
+;; (with-open-file (f "~/work/sicp/img.png")
+;;   (with-canvas (:width 300 :height 300)
+;;     (funcall (make-picture (list
+;; 			    (list (cons 60 30)
+;; 				  (cons 60 270))
+;; 			    (list (cons 60 270)
+;; 				  (cons 240 270)
+;; 				  (cons 240 190)
+;; 				  (cons 60 190))
+;; 			    (list (cons 60 190)
+;; 				  (cons 240 190)
+;; 				  (cons 240 30)
+;; 				  (cons 60 30))))
+;; 	     (make-rectangle 0 0 1 1))
+;;     (save-png f)))
+(defun beside (p1 p2 a)
+  (lambda (rect)
+    (let* ((ax (scale a (horiz rect)))
+	   (comp-ax (scale (- 1 a) (horiz rect)))
+	   (orig2 (add-seg (origin rect) ax)))
+      (funcall p1 (make-rectangle
+		   (car (origin rect))
+		   (cdr (origin rect))		     
+		   ax
+		   (vert rect)))
+      (funcall p2 (make-rectangle
+		   (car orig2)
+		   (cdr orig2)
+		   comp-ax
+		   (vert rect))))))
