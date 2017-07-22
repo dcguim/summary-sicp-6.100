52b83aa498d541dcb4545df2d94010e363afdfbc
Daniel Guimaraes <dcguim@gmail.com>
2017-07-22 17:40:05 -0300

    There is not a proper commit message because this code was not properly tested, there are some gaps left in the understanding of this "AI expert system", what exactly does he mean by "user-inital-enviroment", used in the evaluate function definition, required to build the instantiation component

--- l4.lisp
+++ l4.lisp
@@ -9,7 +9,7 @@
 ;;        (simplifier deriv-rules))
 ;; > (dsimp '(dd (+ x y) x))
 ;; (+ 1 0)
-(defparameter deriv-rules
+(defparameter *deriv-rules*
   '(
     ((dd (?c c) (? v)) 0)
     ((dd (?v v) (? v)) 1)
@@ -37,6 +37,58 @@
 ;;  |-->[match]----dictionary---->[instantiator]-|
 ;;  |                                            |
 ;;  |--------expression-and-dictionary-----------|
+(defun arbitrary-const? (pat)
+  (remove nil
+	  (map 'list
+	       (lambda (x)
+		 (if (and (consp x)
+			  (string= (car x) "?C"))
+		     x
+		     ())) pat)))
+
+(defun arbitrary-var? (pat)
+  (remove nil
+	  (map 'list
+	       (lambda (x)
+		 (if (and (consp x)
+			  (string= (car x) "?V"))
+		     x
+		     ())) pat)))
+
+(defun arbitrary-exp? (pat)
+  (remove nil
+	  (map 'list
+	       (lambda (x)
+		 (if (and (consp x)
+			  (string= (car x) "?"))
+		     x
+		     ())) pat)))
+
+(defun const? (exp)
+  (numberp exp))
+
+(defun var? (exp)
+  (symbolp exp))
+
+(defun empty-dict ()
+  '())
+
+(defun var-name (pat)
+  (cadar (arbitrary-var? pat)))
+  
+
+(defun extend-dict (pat dat dict)
+  (let* ((name (var-name pat))
+	;; assq searches alist dict for value stored in key 'name'
+	(v (assoc name dict)))
+    (cond ((null v)
+	   (acons name dat dict))
+	  ((eq (cadr v) dat) dict)
+	  (t 'failed))))
+
+(defun lookup (var dict)
+  (let ((v (assoc var dict)))
+    (if (null v) var (cadr v))))
 
 (defun match (pat exp dict)
   (cond ((eq dict 'failed) 'failed)
@@ -58,7 +110,49 @@
 	 (extend-dict pat exp dict))	 
 	((not (consp exp)) 'failed)
 	(t (match (cdr pat)
-	     (cdr exp)
-	     (match (car pat)
-	       (car exp)
-	       dict)))))))
+	          (cdr exp)
+		  (match (car pat)
+		         (car exp)
+		         dict)))))))
+
+(defun instantiate (skel dict)
+  (labels ((tree-walk (s)
+	     (cond ((not (consp s)) s)
+		   ((skeleton-evaluation? s)
+		    (evaluate (eval s) dict))
+		   (t (cons (tree-walk (car s))
+			    (tree-walk (cdr s)))))))
+    (tree-walk skel)))
+
+;; What does he mean by "user-initial-enviroment"
+;; (defun evaluate (form dict)
+;;   (if (not (consp form))      
+;;       (lookup form dict)
+;;       (apply
+;;        (eval (lookup (car form) dict)
+;; 	     ;;:user-initial-enviroment)
+
+(defun simplify-exp (exp)
+  (try-rules
+   (if (consp exp)
+       (map 'list #'simplify-exp exp))))
+
+(defun scan (rules)
+  (if (null rules)
+      exp
+      ;; dict is equals the match of the pattern of the top rule, exp
+      ;; and the given dict
+      (let ((dict (match (caar rules) exp (empty-dict))))
+	(if (eq dict 'failed)
+	    (scan (cdr rules)
+		  ;; Simplify the expression resulted by instantiating
+		  ;;  the skeleton of the top rule given the dict
+		  (simplify-exp (instantiate (cadar rules)
+					     dict)))))))
+(defun try-rules (exp)
+  (scan *deriv-rules*))
+				       
+		  
+		    
+					  
+	   
